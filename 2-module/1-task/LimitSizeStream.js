08d397fbe189e3212ca64096c31eaa55bf18baee
ilya <ilya-mir25@mail.ru>
2020-03-23 22:42:12 +0300

    git commit -am "add solution for 2-module 1-task"

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,11 +4,15 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.encoding = options.encoding;
   }
 
   _transform(chunk, encoding, callback) {
-
+    callback(new LimitExceededError, chunk);
+    callback(null, chunk);
   }
 }
+
 
 module.exports = LimitSizeStream;
