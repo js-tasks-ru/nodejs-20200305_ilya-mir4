89e0d3f79c18aa67284bac7644229c35d01d7485
ilya <ilya-mir25@mail.ru>
2020-03-24 18:51:22 +0300

    git commit -am "add solution for 2-module 1-task" git commit -am "add solution for 2-module 2-task"

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,25 +1,34 @@
-const stream = require('stream');
-const os = require('os');
-
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
-    console.warn();
+
+    this.remainder = '';
   }
 
   _transform(chunk, encoding, callback) {
-    console.warn(chunk.toString().split());
-    const currentData = chunk.toString().split(os.EOL);
-    console.warn(currentData);
-    currentData.forEach(data => {
-      console.warn(data);
-      this.push(data)
-    })
-    callback(null);
+    const str = this.remainder + chunk.toString();
+    const lines = str.split(os.EOL);
+    const lastLine = lines.pop();
+    this.remainder = '';
+
+    for (const line of lines) {
+      this.push(line);
+    }
+
+    if (str.endsWith(os.EOL)) {
+      this.push(lastLine);
+    } else {
+      this.remainder = lastLine;
+    }
+
+    callback();
   }
 
-  _flush() {
+  _flush(callback) {
+    if (this.remainder) {
+      this.push(this.remainder);
+    }
+
+    callback();
   }
 }
-
-module.exports = LineSplitStream;
