708dddc6e22ee4fee10ae908e95c9aaa196e6e89
ilya <ilya-mir25@mail.ru>
2020-04-03 01:28:44 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -5,13 +5,41 @@ const app = new Koa();
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
+
+app.use(async (ctx, next) => {
+  return next();
+});
+
 const Router = require('koa-router');
 const router = new Router();
 
+const clients = new Set();
+
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve) => {
+    clients.add(resolve);
+  });
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (!message) {
+    ctx.throw(400, 'required field `message` is missing');
+  }
+
+
+  console.warn(clients);
+
+  clients.forEach((resolve) => {
+    resolve(message);
+  });
+
+  clients.clear();
+
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
